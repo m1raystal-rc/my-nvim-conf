0d6b2fee11f6b92999688ddf42466e48bc5f570c
m1racleur <m1raystal@outlook.com>
2026-05-01 15:40:21 +0800

    update barbar to new version to fix bugs

--- lua/plugins/unity_1.lua
+++ lua/plugins/unity_1.lua
@@ -223,7 +223,7 @@ return {
 	},
 	{
 		"romgrk/barbar.nvim",
-		version = "^1.0.0", -- optional: only update when a new 1.x version is released
+		version = false, -- optional: only update when a new 1.x version is released
 		dependencies = {
 			"lewis6991/gitsigns.nvim",
 			"nvim-tree/nvim-web-devicons",
